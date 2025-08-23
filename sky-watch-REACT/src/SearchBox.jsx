637dc8bb44afd9f7283543d8ebe05975bfb012a1
Aksh Bhatnagar <akshbhatnagar2111@gmail.com>
2025-08-23 09:09:23 +0530

    Update SearchBox.jsx

--- sky-watch-REACT/src/SearchBox.jsx
+++ sky-watch-REACT/src/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({ updateInfo }) {
     let [city, setCity] = useState("");
     let [error, setError] = useState(false);
     const API_URL = `https://api.openweathermap.org/data/2.5/weather`;
-    const API_KEY = "191a59dbc6783b75b8935f296f6f6e2d";
+    const API_KEY = "Enter your Api key";
 
     let getWeatherInfo = async () => {
         try {
